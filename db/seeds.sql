9b756aacca82b87334eaef71dd6bfe98caef59a1
Cory Piette <coryjpiette@gmail.com>
2021-08-15 14:53:19 -0400

    adding db files

---
+++ db/seeds.sql
@@ -0,0 +1,30 @@
+INSERT INTO department (id, name)
+VALUES (1, "Sales"),
+  (2, "Engineering"),
+  (3, "Accounting"),
+  (4, "Legal"),
+  (5, "Management");
+INSERT INTO role (id, title, salary, department_id)
+VALUES (01, "Salesperson", 50000.00, 1),
+  (02, "Engineer", 75000.00, 2),
+  (03, "Accountant", 100000.00, 3),
+  (04, "Lawyer", 150000.00, 4),
+  (05, "Manager", 75000.00, 5);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (001, "Mr.", "Manager", 05, NULL),
+  (002, "Cory", "Piette", 05, NULL),
+  (003, "Bob", "Smith", 01, 001),
+  (004, "Joe", "Schmo", 04, 002),
+  (005, "Jane", "Doe", 03, 002);
+
+DESCRIBE department;
+DESCRIBE role;
+DESCRIBE employee;
+
+SELECT *
+FROM department;
+SELECT *
+FROM employee;
+SELECT *
+FROM role;
--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2')
 const table = require('console.table');
 
-const allPositions = []
 const allManagers = []
+const allRoles = []
+
 
 // Database connect
 const db = mysql.createConnection(
@@ -16,6 +17,27 @@ const db = mysql.createConnection(
     database: 'employee_tracker_db'
   },
   
-  console.log(`Connected to the employee_tracker_db database.`)
+  console.log(`Succesfully connected to employee_tracker_db.`)
 
 );
+
+function initPrompt()
+{
+  inquirer.prompt([
+    {
+      type: "list",
+
+      message: "What would you like to do?",
+      name: "choice",
+      choices: [
+        "View Departments",
+        "View Roles",
+        "View Employees",
+        "Update Employee Information",
+        "Add Employee",
+        "Add Role",
+        "Add Department"
+      ]
+    }
+
+}
